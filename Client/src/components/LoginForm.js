d8511109604e6ce3d0336f13dda7f405954c86fc
Adarsh Sahu <adarshsahu1077@gmail.com>
2024-01-08 23:13:03 +0530

    Login/signup details visible in console

--- Client/src/components/LoginForm.js
+++ Client/src/components/LoginForm.js
@@ -4,39 +4,22 @@ import signinImg from '../assets/signin-image.jpg';
 import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
+    const [formLoginData, setFormLoginData] = useState({
+        userName: '',
+        password: '',
+    });
 
-    const handleUsernameChange = (e) => {
-        setUsername(e.target.value);
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormLoginData({
+            ...formLoginData,
+            [name]: value,
+        });
     };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    };
-
-    const handleLogin = async () => {
-        try {
-            // Make an API call to authenticate the user
-            const response = await fetch('/api/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ username, password }),
-            });
-
-            if (response.ok) {
-                // User successfully logged in
-                console.log('Logged in successfully');
-                // You may redirect the user or perform other actions upon successful login
-            } else {
-                // Handle login failure
-                console.log('Login failed');
-            }
-        } catch (error) {
-            console.error('Error occurred during login:', error);
-        }
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log(formLoginData);
     };
 
     return (
@@ -49,7 +32,7 @@ const LoginForm = () => {
             </div>
 
             <div className="content1">
-                <form action="/login" method="POST" onSubmit={handleLogin}>
+                <form onSubmit={handleLogin}>
                     <div className="user-details-1">
                         <div className="input-box1">
                             <span className="details1">Username</span>
@@ -57,8 +40,8 @@ const LoginForm = () => {
                                 type="text"
                                 placeholder="Enter your username"
                                 name="userName"
-                                value={username}
-                                onChange={handleUsernameChange}
+                                value={formLoginData.username}
+                                onChange={handleInputChange}
                                 required />
                         </div>
                         <div className="input-box1">
@@ -67,8 +50,8 @@ const LoginForm = () => {
                                 type="password"
                                 placeholder="Enter your password"
                                 name="password"
-                                value={password}
-                                onChange={handlePasswordChange}
+                                value={formLoginData.password}
+                                onChange={handleInputChange}
                                 required
                             />
                         </div>
--- Client/src/components/SignupForm.js
+++ Client/src/components/SignupForm.js
@@ -1,61 +1,144 @@
-import React from 'react'
+import React, { useState } from 'react';
 import '../css/SignupForm.css';
 import signupImg from '../assets/signup-image.jpg';
 import { Link } from 'react-router-dom';
 
 const SignupForm = () => {
+    const [formData, setFormData] = useState({
+        fullName: '',
+        userName: '',
+        email: '',
+        phone: '',
+        password: '',
+        confirmPassword: '',
+        gender: '',
+    });
 
-    const handleRegister = () => {
-        // Implement register functionality here or redirect to registration page
-        console.log('Redirecting to registration page');
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
+
+    const handleRegister = (e) => {
+        e.preventDefault();
+        console.log(formData);
     };
 
     return (
         <div className='main-div'>
             <div className="container">
                 <div className="content">
-                    <form action="/register" method="POST"  onSubmit={handleRegister}>
+                    <form onSubmit={handleRegister}>
                         <div className="user-details">
                             <div className="input-box">
                                 <span className="details">Full Name</span>
-                                <input type="text" placeholder="Enter your name" name="fullName" required />
+                                <input
+                                    type="text"
+                                    placeholder="Enter your name"
+                                    name="fullName"
+                                    value={formData.fullName}
+                                    onChange={handleInputChange}
+                                    required
+                                />
                             </div>
                             <div className="input-box">
                                 <span className="details">Username</span>
-                                <input type="text" placeholder="Enter your username" name="userName" required />
+                                <input
+                                    type="text"
+                                    placeholder="Enter your username"
+                                    name="userName"
+                                    value={formData.userName}
+                                    onChange={handleInputChange}
+                                    required
+                                />
                             </div>
                             <div className="input-box">
                                 <span className="details">Email</span>
-                                <input type="text" placeholder="Enter your email" name="email" required />
+                                <input
+                                    type="text"
+                                    placeholder="Enter your email"
+                                    name="email"
+                                    value={formData.email}
+                                    onChange={handleInputChange}
+                                    required
+                                />
                             </div>
                             <div className="input-box">
                                 <span className="details">Phone Number</span>
-                                <input type="text" placeholder="Enter your number" name="phone" required />
+                                <input
+                                    type="text"
+                                    placeholder="Enter your number"
+                                    name="phone"
+                                    value={formData.phone}
+                                    onChange={handleInputChange}
+                                    required
+                                />
                             </div>
                             <div className="input-box">
                                 <span className="details">Password</span>
-                                <input type="password" placeholder="Enter your password" name="password" required />
+                                <input
+                                    type="text"
+                                    placeholder="Enter your password"
+                                    name="password"
+                                    value={formData.password}
+                                    onChange={handleInputChange}
+                                    required
+                                />
                             </div>
                             <div className="input-box">
                                 <span className="details">Confirm Password</span>
-                                <input type="password" placeholder="Confirm your password" name="confirmPassword" required />
+                                <input
+                                    type="text"
+                                    placeholder="Confirm your password"
+                                    name="confirmPassword"
+                                    value={formData.confirmPassword}
+                                    onChange={handleInputChange}
+                                    required
+                                />
                             </div>
                         </div>
                         <div className="gender-details">
-                            <input type="radio" name="gender" id="dot-1" value="Male" />
-                            <input type="radio" name="gender" id="dot-2" value="Female" />
-                            <input type="radio" name="gender" id="dot-3" value="Prefer not to say" />
+                            <input
+                                className='gender-radio'
+                                type="radio"
+                                name="gender"
+                                id="dot-1"
+                                value="Male"
+                                checked={formData.gender === 'Male'}
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                className='gender-radio'
+                                type="radio"
+                                name="gender"
+                                id="dot-2"
+                                value="Female"
+                                checked={formData.gender === 'Female'}
+                                onChange={handleInputChange}
+                            />
+                            <input
+                                className='gender-radio'
+                                type="radio"
+                                name="gender"
+                                id="dot-3"
+                                value="Prefer not to say"
+                                checked={formData.gender === 'Prefer not to say'}
+                                onChange={handleInputChange}
+                            />
                             <span class="gender-title">Gender</span>
                             <div className="category">
-                                <label for="dot-1">
+                                <label htmlFor="dot-1">
                                     <span className="dot one"></span>
                                     <span className="gender">Male</span>
                                 </label>
-                                <label for="dot-2">
+                                <label htmlFor="dot-2">
                                     <span className="dot two"></span>
                                     <span className="gender">Female</span>
                                 </label>
-                                <label for="dot-3">
+                                <label htmlFor="dot-3">
                                     <span className="dot three"></span>
                                     <span className="gender">Prefer not to say</span>
                                 </label>
